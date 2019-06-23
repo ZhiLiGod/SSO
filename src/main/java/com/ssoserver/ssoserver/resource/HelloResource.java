01dd493f994c107f27c5fc048ccc3ba376a27311
Zhi Li <zhi.li@invoicefair.com>
2019-06-23 14:01:16 +0100

    authorization server config

---
+++ src/main/java/com/ssoserver/ssoserver/resource/HelloResource.java
@@ -0,0 +1,16 @@
+package com.ssoserver.ssoserver.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/hello")
+public class HelloResource {
+
+  @GetMapping
+  public String hello() {
+    return "Hello World";
+  }
+
+}
